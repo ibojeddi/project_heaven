da8acd3afdea4a8a79b838a16b1f6d54ed6fdc2c
abrahamjeddi <abrahamjeddi@gmail.com>
2016-11-18 21:22:25 -0800

    lists Cemeteries successfully as HL

--- heaven/views.py
+++ heaven/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Cemetery
+from django.utils import timezone
+
 
 def cemetery_list(request):
-    return render(request, 'heaven/cemetery_list.html', {})
+    cemeteries=Cemetery.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
+    dates_only=Cemetery.objects.filter(date_created__lte=timezone.now())
+    return render(request, 'heaven/cemetery_list.html', {'cemeteries':cemeteries})
